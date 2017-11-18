9713d61e95e3b33a52035e9f4c7fd1b24b79ac03
John Hawthorn <john.hawthorn@gmail.com>
2017-11-18 02:00:27 -0800

    Add all assets

---
+++ assets/levels/tiles.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="magnoman" tilewidth="8" tileheight="8" tilecount="80" columns="8">
+ <image source="../images/tiles.png" width="64" height="80"/>
+ <tile id="13" type="grip"/>
+ <tile id="14" type="grip"/>
+</tileset>
