ca1990f28ab36f3bede3f19659ce8f11a7501070
John Hawthorn <john.hawthorn@gmail.com>
2017-11-19 01:47:34 -0800

    Deploy 2017-11-19 01:47:33

--- assets/levels/tiles.tsx
+++ assets/levels/tiles.tsx
@@ -1,6 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="magnoman" tilewidth="8" tileheight="8" tilecount="80" columns="8">
  <image source="../images/tiles.png" width="64" height="80"/>
- <tile id="13" type="grip"/>
- <tile id="14" type="grip"/>
+ <tile id="13">
+  <properties>
+   <property name="grip" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="grip" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
